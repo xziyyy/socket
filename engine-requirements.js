ac59394572ffe521c51dce60bd6882ffaad71e36
Aldi Fauzi <fauzine704@gmail.com>
2025-07-26 04:43:53 +0700

    Update engine-requirements.js

--- engine-requirements.js
+++ engine-requirements.js
@@ -1,5 +1,5 @@
 const major = parseInt(process.versions.node.split('.')[0], 10)
 if (major < 20) {
-console.error(`nodejs must v20, you cannot run this script`)
+console.error(`nodejs must v20, you cannot run this script, and run again`)
 process.exit(1)
 }
